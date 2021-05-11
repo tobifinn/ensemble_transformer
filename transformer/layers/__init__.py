82fea8890c07ea1046f46c82d56163d29a3f31f4
Tobias Finn <tobias.sebastian.finn@uni-hamburg.de>
2021-05-11 22:03:19 +0200

    Added dictionary about available transformers

--- transformer/layers/__init__.py
+++ transformer/layers/__init__.py
@@ -2,3 +2,10 @@ from .softmax_transformer import SoftmaxTransformer
 from .kernel_transformer import KernelTransformer
 from .ens_transformer import EnsTransformer
 from .conv import EarthPadding, EnsConv2d
+
+
+avail_transformers = {
+    'softmax': SoftmaxTransformer,
+    'kernel': KernelTransformer,
+    'ensemble': EnsTransformer
+}
