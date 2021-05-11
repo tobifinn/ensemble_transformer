7b724d7ac3100bbcd2fa832d5dd576e7e67c108c
Tobias Finn <tobias.sebastian.finn@uni-hamburg.de>
2021-05-11 22:02:24 +0200

    Added init to layers

---
+++ transformer/layers/__init__.py
@@ -0,0 +1,4 @@
+from .softmax_transformer import SoftmaxTransformer
+from .kernel_transformer import KernelTransformer
+from .ens_transformer import EnsTransformer
+from .conv import EarthPadding, EnsConv2d
