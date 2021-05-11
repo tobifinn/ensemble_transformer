ce832327b0badb9b0b3df17e0ef9fce8a7b31feb
Tobias Finn <tobias.sebastian.finn@uni-hamburg.de>
2021-05-11 18:38:48 +0200

    Added layer utilities to move ensemble dimension to batch dimension

---
+++ transformer/layers/utils.py
@@ -0,0 +1,45 @@
+#!/bin/env python
+# -*- coding: utf-8 -*-
+#
+# Created on 11.05.21
+#
+# Created for ensemble_transformer
+#
+# @author: Tobias Sebastian Finn, tobias.sebastian.finn@uni-hamburg.de
+#
+#    Copyright (C) {2021}  {Tobias Sebastian Finn}
+
+
+# System modules
+import logging
+
+# External modules
+import torch
+
+# Internal modules
+
+
+logger = logging.getLogger(__name__)
+
+
+def ens_to_batch(in_tensor: torch.Tensor) -> torch.Tensor:
+    try:
+        out_tensor = in_tensor.view(-1, *in_tensor.shape[-3:])
+    except RuntimeError:
+        out_tensor = in_tensor.reshape(-1, *in_tensor.shape[-3:]).contiguous()
+    return out_tensor
+
+
+def split_batch_ens(
+        in_tensor: torch.Tensor,
+        like_tensor: torch.Tensor
+) -> torch.Tensor:
+    try:
+        out_tensor = in_tensor.view(
+            *like_tensor.shape[:-3], *in_tensor.shape[-3:]
+        )
+    except RuntimeError:
+        out_tensor = in_tensor.reshape(
+            *like_tensor.shape[:-3], *in_tensor.shape[-3:]
+        ).contiguous()
+    return out_tensor
