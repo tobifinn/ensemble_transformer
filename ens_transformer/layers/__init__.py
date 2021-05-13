b80318231096692c82460b5971d59b3a3f6ef45e
Tobias Finn <tobias.sebastian.finn@uni-hamburg.de>
2021-05-13 18:20:00 +0200

    Simplified layers

--- ens_transformer/layers/__init__.py
+++ ens_transformer/layers/__init__.py
@@ -1,13 +1,8 @@
-from .softmax_transformer import SoftmaxTransformer
-from .kernel_transformer import KernelTransformer
-from .ens_transformer import EnsTransformer
-from .conv import EarthPadding, EnsConv2d
-from .activations import avail_activations
-from .embedding import avail_embeddings
+from .activations import *
+from .conv import *
+from .padding import *
 
 
-avail_transformers = {
-    'softmax': SoftmaxTransformer,
-    'kernel': KernelTransformer,
-    'ensemble': EnsTransformer
-}
+__all__ = [
+    'EarthPadding', 'EnsConv2d', 'SELUKernel'
+]
