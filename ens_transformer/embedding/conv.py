213a9c9ea19597329f22b572111f0ac1a18e21dd
Tobias Finn <tobias.sebastian.finn@uni-hamburg.de>
2021-05-13 18:26:54 +0200

    Added convolutional embedding

--- ens_transformer/embedding/conv.py
+++ ens_transformer/embedding/conv.py
@@ -1,7 +1,7 @@
 #!/bin/env python
 # -*- coding: utf-8 -*-
 #
-# Created on 12.05.21
+# Created on 13.05.21
 #
 # Created for ensemble_transformer
 #
@@ -15,67 +15,15 @@ import logging
 from typing import Tuple
 
 # External modules
-import torch.nn
+import torch
+from hydra.utils import get_class
 
 # Internal modules
-from .activations import avail_activations
-from .conv import EarthPadding, EnsConv2d
-from .utils import EnsembleWrapper
+from ..utils import EnsembleWrapper
+from ..layers import EnsConv2d, EarthPadding
 
 
 logger = logging.getLogger(__name__)
-
-
-class LinearEmbedding(torch.nn.Module):
-    def __init__(
-            self,
-            input_size: int = 6144,
-            embedding_size: int = 256
-    ):
-        super().__init__()
-        self.input_size = input_size
-        self.embedding_layer = torch.nn.Linear(
-            in_features=input_size, out_features=embedding_size, bias=False
-        )
-
-    def forward(self, in_tensor: torch.Tensor) -> torch.Tensor:
-        in_embed_tensor = in_tensor.view(
-            *in_tensor.shape[:2], self.input_size
-        )
-        output_tensor = self.embedding_layer(in_embed_tensor)
-        return output_tensor
-
-
-class ShallowEmbedding(torch.nn.Module):
-    def __init__(
-            self,
-            input_size: int = 6144,
-            hidden_neurons: int = 1024,
-            embedding_size: int = 256,
-            activation: str = 'selu'
-    ):
-        super().__init__()
-        self.input_size = input_size
-        self.embedding_layer = torch.nn.Sequential(
-            torch.nn.Linear(
-                in_features=input_size,
-                out_features=hidden_neurons,
-                bias=True
-            ),
-            avail_activations[activation],
-            torch.nn.Linear(
-                in_features=hidden_neurons,
-                out_features=embedding_size,
-                bias=True
-            )
-        )
-
-    def forward(self, in_tensor: torch.Tensor) -> torch.Tensor:
-        in_embed_tensor = in_tensor.view(
-            *in_tensor.shape[:2], self.input_size
-        )
-        output_tensor = self.embedding_layer(in_embed_tensor)
-        return output_tensor
 
 
 class ConvEmbedding(torch.nn.Module):
@@ -85,7 +33,7 @@ class ConvEmbedding(torch.nn.Module):
             n_conv_layers: int = 6,
             n_init_features: int = 4,
             embedding_size: int = 256,
-            activation: str = 'selu',
+            activation: str = 'torch.nn.SELU',
             dim_sizes: Tuple[int, int] = (32, 64)
     ):
         super().__init__()
@@ -107,7 +55,7 @@ class ConvEmbedding(torch.nn.Module):
             in_channels: int = 3,
             n_conv_layers: int = 6,
             n_init_features: int = 4,
-            activation: str = 'selu',
+            activation: str = 'torch.nn.SELU',
             dim_sizes: Tuple[int, int] = (32, 64)
     ) -> Tuple[torch.nn.Sequential, int]:
         curr_in_channels = in_channels
@@ -126,7 +74,7 @@ class ConvEmbedding(torch.nn.Module):
                         out_channels=curr_out_channels,
                         kernel_size=5
                     ),
-                    avail_activations[activation](inplace=True)
+                    get_class(activation)(inplace=True)
                 )
             )
             curr_in_channels = curr_out_channels
@@ -145,10 +93,3 @@ class ConvEmbedding(torch.nn.Module):
         )
         embedded_tensor = self.output_layer(curr_tensor)
         return embedded_tensor
-
-
-avail_embeddings = {
-    'linear': LinearEmbedding,
-    'shallow': ShallowEmbedding,
-    'conv': ConvEmbedding
-}
