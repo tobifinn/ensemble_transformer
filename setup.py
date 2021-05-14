006c5df7690d3cc92e1b7aea2bc304ec3c5cb17a
Tobias Finn <tobias.sebastian.finn@uni-hamburg.de>
2021-05-14 18:22:59 +0200

    Renamed package

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import find_packages, setup
 
 setup(
-    name='src',
+    name='ensemble_transformer',
     packages=find_packages(),
     version='0.1.0',
     description='The ensemble transformer is a neural network module for processing of ensemble weather forecasting data',
