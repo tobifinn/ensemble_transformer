0f6136b1247ba7a93c579c0418255d9f51d8a3cd
Tobias Finn <tobias.sebastian.finn@uni-hamburg.de>
2021-05-14 16:22:17 +0200

    Added the ensemble and kernel transformer to the init

--- ens_transformer/transformers/__init__.py
+++ ens_transformer/transformers/__init__.py
@@ -1,3 +1,5 @@
+from .ens_transformer import *
+from .kernel_transformer import *
 from .softmax_transformer import *
 
-__all__ = ['SoftmaxTransformer']
+__all__ = ['EnsTransformer', 'KernelTransformer', 'SoftmaxTransformer']
