613e08ec3579a5e3eecd0f80d201530bcc91659b
Tobias Finn <tobias.sebastian.finn@uni-hamburg.de>
2021-05-20 11:45:47 +0200

    Added direct neural network

---
+++ ens_transformer/models/direct_net.py
@@ -0,0 +1,45 @@
+#!/bin/env python
+# -*- coding: utf-8 -*-
+#
+# Created on 20.05.21
+#
+# Created for ensemble_transformer
+#
+# @author: Tobias Sebastian Finn, tobias.sebastian.finn@uni-hamburg.de
+#
+#    Copyright (C) {2021}  {Tobias Sebastian Finn}
+
+
+# System modules
+import logging
+
+# External modules
+import torch
+from omegaconf import DictConfig
+from hydra.utils import get_class
+
+# Internal modules
+from .base_net import BaseNet
+from ..layers import EnsConv2d, EarthPadding
+
+logger = logging.getLogger(__name__)
+
+
+class DirectNet(BaseNet):
+    @staticmethod
+    def _init_transformers(
+            cfg: DictConfig,
+            hidden_channels: int = 64,
+            n_transformers: int = 1
+    ) -> torch.nn.Sequential:
+        transformer_list = []
+        for idx in range(n_transformers):
+            submodule = torch.nn.Sequential(
+                EarthPadding((cfg.kernel_size-1)//2),
+                EnsConv2d(hidden_channels, hidden_channels,
+                          kernel_size=cfg.kernel_size, padding=0),
+                get_class(cfg.activation)(inplace=True)
+            )
+            transformer_list.append(submodule)
+        transformers = torch.nn.Sequential(*transformer_list)
+        return transformers
