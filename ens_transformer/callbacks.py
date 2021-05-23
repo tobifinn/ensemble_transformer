2b710be13d640fa007b4387b1258eb3afff32cc8
Tobias Finn <tobias.sebastian.finn@uni-hamburg.de>
2021-05-23 13:16:32 +0200

    Added possibility to store model before start

--- ens_transformer/callbacks.py
+++ ens_transformer/callbacks.py
@@ -12,6 +12,9 @@
 
 # System modules
 import logging
+from pathlib import Path
+import os
+from typing import Optional, Union
 
 # External modules
 from pytorch_lightning.callbacks import Callback
@@ -25,8 +28,12 @@ logger = logging.getLogger(__name__)
 
 class ValidationOnStartCallback(Callback):
     # Based on https://github.com/PyTorchLightning/pytorch-lightning/issues/1715#issuecomment-642480058
-    def __init__(self):
-        pass
+    def __init__(self, dirpath: Optional[Union[str, Path]] = None):
+        super().__init__()
+        self.dirpath = dirpath
 
     def on_train_start(self, trainer: Trainer, pl_module):
+        if self.dirpath is not None:
+            ckpt_path = os.path.join(self.dirpath, 'before_train.ckpt')
+            trainer.save_checkpoint(ckpt_path)
         return trainer.run_evaluation()
