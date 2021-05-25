58b18a92f95b08224477261f748ce8df71735b6e
Tobias Finn <tobias.sebastian.finn@uni-hamburg.de>
2021-05-25 09:37:27 +0200

    Added residual layer to init of layers

--- ens_transformer/layers/__init__.py
+++ ens_transformer/layers/__init__.py
@@ -1,8 +1,9 @@
 from .activations import *
 from .conv import *
 from .padding import *
+from .residual import *
 
 
 __all__ = [
-    'EarthPadding', 'EnsConv2d', 'ELUKernel'
+    'EarthPadding', 'EnsConv2d', 'ELUKernel', 'ResidualLayer'
 ]
