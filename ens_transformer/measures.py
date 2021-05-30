2cbdc7dc984acbc4263b43250a847887143a3507
Tobias Finn <tobias.sebastian.finn@uni-hamburg.de>
2021-05-30 17:42:22 +0200

    Removed old GPL license from measures.py file

--- ens_transformer/measures.py
+++ ens_transformer/measures.py
@@ -3,25 +3,12 @@
 #
 # Created on 04.09.20
 #
-# Created for 20_neurips_vaeda
+# Created for ensemble_transformer
 #
 # @author: Tobias Sebastian Finn, tobias.sebastian.finn@uni-hamburg.de
 #
 #    Copyright (C) {2020}  {Tobias Sebastian Finn}
-#
-#    This program is free software: you can redistribute it and/or modify
-#    it under the terms of the GNU General Public License as published by
-#    the Free Software Foundation, either version 3 of the License, or
-#    (at your option) any later version.
-#
-#    This program is distributed in the hope that it will be useful,
-#    but WITHOUT ANY WARRANTY; without even the implied warranty of
-#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#    GNU General Public License for more details.
-#
-#    You should have received a copy of the GNU General Public License
-#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
-#
+
 
 # System modules
 import logging
