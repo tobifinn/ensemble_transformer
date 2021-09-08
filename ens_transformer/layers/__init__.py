cf30d18572c4c82044c8aac653342401128ebddd
Tobias Finn <tobias.finn@enpc.fr>
2021-09-08 10:44:47 +0200

    Added moved layers to init

--- ens_transformer/layers/__init__.py
+++ ens_transformer/layers/__init__.py
@@ -1,9 +1,12 @@
 from .activations import *
 from .conv import *
+from .ens_wrapper import *
+from .model_embedding import *
 from .padding import *
 from .residual import *
 
 
 __all__ = [
-    'EarthPadding', 'EnsConv2d', 'ELUKernel', 'ResidualLayer'
+    'EarthPadding', 'EnsConv2d', 'ELUKernel', 'ResidualLayer',
+    'ModelEmbedding', 'EnsembleWrapper'
 ]
